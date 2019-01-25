31b14b9b4e5423042a956d9e944cebebc23b5ff2
Ahmet Yildiz <36455837+aviatus@users.noreply.github.com>
2019-01-25 16:17:07 +0300

    Add files via upload

---
+++ AStarSearch/App.java
@@ -0,0 +1,12 @@
+package AStarSearch;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		AStarAlgorithm algorithm = new AStarAlgorithm();
+		algorithm.search();
+		algorithm.showPath();
+		
+	}
+}
